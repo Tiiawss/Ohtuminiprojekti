25f53245d027724ea5a4d14b2c83247631d10fec
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-11-22 10:49:19 +0200

    Book tests continue

--- src/tests/book_repo_test.py
+++ src/tests/book_repo_test.py
@@ -10,16 +10,40 @@ class TestBookRepository(unittest.TestCase):
         self.assertEqual(self.book_repository.books, [])
 
     def test_add_new_book(self):
-        self.book_repository.add_book("book")
+        self.book_repository.add_book(book = {
+            "id": "1",
+            "author": "Tee",
+            "title": "Teeskentelyä",
+            "year": "2022",
+            "publisher": "Paras"
+            }
+        )
 
         self.assertEqual(len(self.book_repository.books), 1)
-        self.assertEqual(self.book_repository.books[0], "book")
+        self.assertEqual(self.book_repository.books[0]["id"], "1")
+        self.assertEqual(self.book_repository.books[0]["title"], "Teeskentelyä")
 
     def test_return_books_correctly(self):
-        self.book_repository.add_book("book")
-        self.book_repository.add_book("book2")
+        self.book_repository.add_book(
+            book = {
+                "id": "1",
+                "author": "Tee",
+                "title": "Teeskentelyä",
+                "year": "2022",
+                "publisher": "Paras"
+            }
+        )
+        self.book_repository.add_book(
+            book = {
+                "id": "2",
+                "author": "Tee",
+                "title": "Teeskentelyä2",
+                "year": "2022",
+                "publisher": "Paras"
+            }
+        )
 
         books = self.book_repository.get_books()
 
         self.assertEqual(len(books), 2)
-        self.assertEqual(books[0], "book")
+        self.assertEqual(books[0]["id"], "1")
