a23e4347c99ae0e31c0b486265c6492b8ae20569
desideri <mr.desipeli@gmail.com>
2022-11-22 09:51:12 +0200

    Luotu

---
+++ src/services/book_citation_service.py
@@ -0,0 +1,14 @@
+from urllib import request
+from flask import requests
+
+
+class bookCitation:
+    def save_citation(self, citation):
+        author = request.form["author"]
+        title = request.form["title"]
+        year = request.form["year"]
+        publisher = request.form["publisher"]
+
+        if author and title and year and publisher:
+            # Lähetä eteenpäin
+            pass
