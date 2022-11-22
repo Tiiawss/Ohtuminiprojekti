fbd4de249deb8c7f745ff5b88443ae5b2830fd48
Tiiawss <tiia.nylen@gmail.com>
2022-11-22 09:46:48 +0200

    html siirretty templates

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,6 @@
 from app import app
+from flask import redirect, render_template, request, session
 
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    return render_template("index.html")
