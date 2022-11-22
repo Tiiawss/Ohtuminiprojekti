b23a4f032d7045fd9dd3059cbb10ff9081bc4567
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-11-22 10:38:54 +0200

    Book repo tests

--- src/tests/book_repo_test.py
+++ src/tests/book_repo_test.py
@@ -1,10 +1,25 @@
 import unittest
 from repositories.book_repository import book_repository
 
-class TestBookRepository(unittest):
-    def __init__(self):
+class TestBookRepository(unittest.TestCase):
+    def setUp(self):
         self.book_repository = book_repository
+        self.book_repository.books = []
 
     def test_new_book_list_is_empty(self):
         self.assertEqual(self.book_repository.books, [])
 
+    def test_add_new_book(self):
+        self.book_repository.add_book("book")
+
+        self.assertEqual(len(self.book_repository.books), 1)
+        self.assertEqual(self.book_repository.books[0], "book")
+
+    def test_return_books_correctly(self):
+        self.book_repository.add_book("book")
+        self.book_repository.add_book("book2")
+
+        books = self.book_repository.get_books()
+
+        self.assertEqual(len(books), 2)
+        self.assertEqual(books[0], "book")
