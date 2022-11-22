37fac96daba2d3f83e366c1cdcd6d9c978c1620a
Tiiawss <tiia.nylen@gmail.com>
2022-11-22 09:50:26 +0200

    form html toimii etusivulta sinne siirryttäessä

--- src/routes.py
+++ src/routes.py
@@ -4,3 +4,7 @@ from flask import redirect, render_template, request, session
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
