cd7791b641b8a6ceb1b9a207e26b10eca77165f6
desideri <mr.desipeli@gmail.com>
2022-11-22 10:33:06 +0200

    lint

--- src/services/book_citation_service.py
+++ src/services/book_citation_service.py
@@ -1,16 +1,17 @@
 from repositories.book_repository import book_repository
 
 
-class bookCitation:
+class BookCitation:
+    """ Book Service """
     def __init__(self, bookrepository = book_repository) -> None:
         self.repo = bookrepository
 
-    def save_citation(self, id, author, title, year, publisher):
+    def save_citation(self, bookid, author, title, year, publisher):
         """ Save book to repository """
 
-        if id and author and title and year and publisher:
+        if bookid and author and title and year and publisher:
             book = {
-            "id": id,
+            "id": bookid,
             "author": author,
             "title": title,
             "year": year,
