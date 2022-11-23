fee9a0e05082663860328c976dc4635c9c7ea6b2
desideri <mr.desipeli@gmail.com>
2022-11-23 09:43:16 +0200

    use coverage

--- tasks.py
+++ tasks.py
@@ -6,9 +6,17 @@ def start(ctx):
     ctx.run("flask --app src/app.py run", pty=True)
 
 @task
+def dev(ctx):
+    ctx.run("flask --app src/app.py --debug run", pty=True)
+
+@task
 def lint(ctx):
     ctx.run("pylint src", pty=True)
 
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src; coverage html", pty=True)
