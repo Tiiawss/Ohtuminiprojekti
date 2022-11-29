a42a5c35aa533b224692ca390ef384e0e83744f5
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-11-29 10:02:14 +0200

    Configuration file created

---
+++ src/repositories/configuration_repository.py
@@ -0,0 +1,33 @@
+import toml
+
+class ConfigurationRepository:
+    """Luokka, joka hakee sovellusta varten tiedot lähdeviittausten kentistä
+    """
+
+    def __init__(self) -> None:
+        """Alustaa luokan 
+        """
+        
+        self._cites = {}
+        self.cites_fields()
+
+    def cites_fields(self):
+        """Hakee konfiguraatio tiedostosta kaikki viittausmuodot
+
+        Template:
+            [Citation type]
+            field_id = [UI name, required bool]
+            field_id_2 = [UI name, required bool]
+            ...
+        """
+
+        content = toml.load("cite_types.toml")
+
+        for typ, fields in content.items():
+            self._cites[typ] = {}
+            for field, values in fields.items():
+                self._cites[typ][field] = (values[0], bool(values[1]))
+
+        print(self._cites)
+
+configuration_repository = ConfigurationRepository()
