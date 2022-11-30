52376ea1d06fb04d84e93bd91867892556252bcf
Tiiawss <tiia.nylen@gmail.com>
2022-11-30 10:43:08 +0200

    test adapted for cite_key edit

--- src/tests/services/book_service_test.py
+++ src/tests/services/book_service_test.py
@@ -31,12 +31,13 @@ class TestBookService(unittest.TestCase):
         self.book_service = BookCitation(StudBookRepo())
 
     def test_saves_citation_correclty(self):
-        re = self.book_service.save_citation(
-            "Tee",
-            "Teeskentelyä",
-            "2022",
-            "Paras"
-        )
+        
+        re = self.book_service.save_citation([
+        ("author", "tee"),
+        ("title", "teee"), 
+        ("year","2002"), 
+        ("publisher","jtn")
+        ])
 
         books = self.book_service.get_all()
 
@@ -45,13 +46,13 @@ class TestBookService(unittest.TestCase):
         self.assertEqual(re, True)
 
     def test_does_not_save_incorrect_citation(self):
-        re = self.book_service.save_citation(
-            "",
-            "Teeskentelyä",
-            "2022",
-            "Paras"
-        )
-
+        re = self.book_service.save_citation([
+        ("author", ""),
+        ("title", "teee"),
+        ("year","2002"),
+        ("publisher","jtn")
+        ])
+        
         books = self.book_service.get_all()
 
         self.assertEqual(re, False)
@@ -60,18 +61,20 @@ class TestBookService(unittest.TestCase):
 
     def test_unique_citekey_proper_formatting(self):
 
-        self.book_service.save_citation(
-            "Pek pekka",
-            "pekan seikkailut",
-            "20",
-            "pekan omakustanne"
-        )
-        self.book_service.save_citation(
-            "Pekpekka",
-            "pekan seikkailut",
-            "2020",
-            "pekan omakustanne"
-        )
+        self.book_service.save_citation([
+        ("author", "pekpekka"),
+        ("title", "teee"),
+        ("year","2002"),
+        ("publisher","jtn")
+        ])
+
+        self.book_service.save_citation([
+        ("author", "pekpekka"),
+        ("title", "teee"),
+        ("year","2002"),
+        ("publisher","jtn")
+        ])
+
         for book in self.book_service.get_all():
             for character in book["cite_key"]:
                 self.assertEqual(
@@ -81,18 +84,18 @@ class TestBookService(unittest.TestCase):
 
     def test_unique_really_unique(self):
 
-        self.book_service.save_citation(
-            "Pek pekka",
-            "pekan seikkailut",
-            "20",
-            "pekan omakustanne"
-        )
-        self.book_service.save_citation(
-            "Pek pekka",
-            "pekan seikkailut",
-            "20",
-            "pekan omakustanne"
-        )
+        self.book_service.save_citation([
+        ("author", "pekpekka"),
+        ("title", "teee"),
+        ("year","2002"),
+        ("publisher","jtn")
+        ])
+        self.book_service.save_citation([
+        ("author", "pekpekka"),
+        ("title", "teee"),
+        ("year","2002"),
+        ("publisher","jtn")
+        ])
         self.assertNotEqual(
             self.book_service.get_all()[0]["cite_key"],
             self.book_service.get_all()[1]["cite_key"]
@@ -100,18 +103,18 @@ class TestBookService(unittest.TestCase):
 
     def test_get_last(self):
 
-        self.book_service.save_citation(
-            "joku",
-            "Teeskentelyä",
-            "2022",
-            "Paras"
-        )
-        self.book_service.save_citation(
-            "joku",
-            "Teeskentelyä",
-            "2022",
-            "Paras"
-        )
+        self.book_service.save_citation([
+        ("author", "pekpekka"),
+        ("title", "teee"),
+        ("year","2002"),
+        ("publisher","jtn")
+        ])
+        self.book_service.save_citation([
+        ("author", "pekpekka2"),
+        ("title", "teee"),
+        ("year","2002"),
+        ("publisher","jtn")
+        ])
 
         book = self.book_service.get_last()
         self.assertEqual(
@@ -126,18 +129,18 @@ class TestBookService(unittest.TestCase):
 
     def test_remove_citation(self):
 
-        self.book_service.save_citation(
-            "Pek Pekka",
-            "Pekan seikkalut",
-            "20",
-            "Oma kustannus"
-        )
-        self.book_service.save_citation(
-            "Pek Pekka P",
-            "Pekan seikkalut 2",
-            "20",
-            "Oma kustannus"
-        )
+        self.book_service.save_citation([
+        ("author", "pekpekka2"),
+        ("title", "teee"),
+        ("year","2002"),
+        ("publisher","jtn")
+        ])
+        self.book_service.save_citation([
+        ("author", "pekpekka1"),
+        ("title", "teee"),
+        ("year","2002"),
+        ("publisher","jtn")
+        ])
 
         citekey = self.book_service.get_all()[0]['cite_key']
         self.book_service.remove_citation(citekey)
