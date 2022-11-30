2890d9d707f66e4291a2622650b23c692e5e9fc8
Tiiawss <tiia.nylen@gmail.com>
2022-11-30 09:59:49 +0200

    test remove book

--- src/tests/repositories/book_repo_test.py
+++ src/tests/repositories/book_repo_test.py
@@ -12,7 +12,7 @@ class TestBookRepository(unittest.TestCase):
 
     def test_add_new_book(self):
         self.book_repository.add_book(book={
-            "id": "1",
+            "cite_key": "1",
             "author": "Tee",
             "title": "Teeskentelyä",
             "year": "2022",
@@ -21,14 +21,14 @@ class TestBookRepository(unittest.TestCase):
         )
 
         self.assertEqual(len(self.book_repository.books), 1)
-        self.assertEqual(self.book_repository.books[0]["id"], "1")
+        self.assertEqual(self.book_repository.books[0]["cite_key"], "1")
         self.assertEqual(
             self.book_repository.books[0]["title"], "Teeskentelyä")
 
     def test_return_books_correctly(self):
         self.book_repository.add_book(
             book={
-                "id": "1",
+                "cite_key": "1",
                 "author": "Tee",
                 "title": "Teeskentelyä",
                 "year": "2022",
@@ -37,7 +37,7 @@ class TestBookRepository(unittest.TestCase):
         )
         self.book_repository.add_book(
             book={
-                "id": "2",
+                "cite_key": "2",
                 "author": "Tee",
                 "title": "Teeskentelyä2",
                 "year": "2022",
@@ -48,4 +48,32 @@ class TestBookRepository(unittest.TestCase):
         books = self.book_repository.get_books()
 
         self.assertEqual(len(books), 2)
-        self.assertEqual(books[0]["id"], "1")
+        self.assertEqual(books[0]["cite_key"], "1")
+
+    def test_remove_book(self):
+        self.book_repository.add_book(
+            book={
+                "cite_key": "1",
+                "author": "Tee",
+                "title": "Teeskentelyä",
+                "year": "2022",
+                "publisher": "Paras"
+            }
+        )
+        self.book_repository.add_book(
+            book={
+                "cite_key": "2",
+                "author": "Tee",
+                "title": "Teeskentelyä2",
+                "year": "2022",
+                "publisher": "Paras"
+            }
+        )
+        books = self.book_repository.get_books()
+        cite_key = self.book_repository.books[0]["cite_key"]
+        self.book_repository.remove_book(cite_key)
+        self.assertEqual(len(books), 1)
+
+        
+
+
