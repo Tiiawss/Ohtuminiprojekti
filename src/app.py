bc36f98d92ce3345a08b7855a3e0268017a73084
Johannes Elonheimo <johannes.elonheimo@gmail.com>
2022-12-10 18:41:43 +0200

    alter app.py import order

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
 import routes  # pylint: disable=unused-import, wrong-import-position, cyclic-import
+from flask import Flask  # pylint: disable=wrong-import-order
 
 app = Flask(__name__)
