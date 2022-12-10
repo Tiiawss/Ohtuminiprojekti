93fcfdac36b54869a1dace90051edd6fbd9a3dfd
Johannes Elonheimo <johannes.elonheimo@gmail.com>
2022-12-10 19:10:11 +0200

    fix routes imported before app init

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-import routes  # pylint: disable=unused-import, wrong-import-position, cyclic-import
 from flask import Flask  # pylint: disable=wrong-import-order
 
 app = Flask(__name__)
+import routes  # pylint: disable=unused-import, wrong-import-position, cyclic-import
+# keep routes under flask
